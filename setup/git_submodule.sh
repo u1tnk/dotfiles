e8a35b8626335611c1bfffa0beb7fd5919263342
yuichi tanaka <u1tnk1123@gmail.com>
2011-04-27 19:08:44 +0900

    dotfilesセットアップ用スクリプト整備

---
+++ setup/git_submodule.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+cd ~/dotfiles
+git submodule init
+git submodule update
+#この後vimを立ち上げて:BundleInstallしてvundler経由でpluginインストールすること
---
+++ setup/vimproc.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+cd ~/.vim/bundle/vimproc
+make -f make_gcc.mak

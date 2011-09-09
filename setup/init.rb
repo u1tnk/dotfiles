e5ef63be0e22ec0788d863734334b08a9ff601f0
yuichi tanaka <u1tnk1123@gmail.com>
2011-09-09 09:28:19 +0900

    setupスクリプトをrubyで記述

---
+++ setup/init.rb
@@ -0,0 +1,21 @@
+#! /usr/bin/env ruby
+# -*- coding: utf-8 -*-
+require "fileutils"
+
+home = ENV['HOME']
+FileUtils.cd home
+File.symlink "dotfiles/.gemrc", ".gemrc" unless FileTest.exist? ".gemrc"
+File.symlink "dotfiles/.gitconfig", ".gitconfig" unless FileTest.exist? ".gitconfig"
+File.symlink "dotfiles/.tmux.conf", ".tmux.conf" unless FileTest.exist? ".tmux.conf"
+File.symlink "dotfiles/.gvimrc", ".gvimrc" unless FileTest.exist? ".gvimrc"
+File.symlink "dotfiles/.vrapperrc", ".vrapperrc" unless FileTest.exist? ".vrapperrc"
+
+File.copy "dotfiles/setup/template/.zshrc", ".zshrc" unless FileTest.exist? ".zshrc"
+File.copy "dotfiles/setup/template/.vimrc", ".vimrc" unless FileTest.exist? ".vimrc"
+
+`sh dotfiles/setup/git_submodule.sh`
+
+#BundleInstallした後vimproc.shを実行
+`sh dotfiles/setup/vimproc.sh` if FileTest.exist? ".vim/bundle"
+
+

98ff86c69e6cfdac418f3173821564baa96aceb3
yuichi tanaka <u1tnk1123@gmail.com>
2017-10-10 23:19:42 +0900

    update clean music

--- scripts/clean_duplicate_music.py
+++ scripts/clean_duplicate_music.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+
 from gmusicapi import Mobileclient
 
 api = Mobileclient()
-api.login('hoge@gmail.com', 'xxxxxxx', Mobileclient.FROM_MAC_ADDRESS)
+api.login('hoge@gmail.com', 'xxxxx', Mobileclient.FROM_MAC_ADDRESS)
 
 library = api.get_all_songs()
-nonFilteringSong = []
 filteringSong = []
 overlapSong = []
 
-for i in library:
-    song = {'title':i['title'], 'album': i['album'], 'trackNumber': i['trackNumber']}
-    nonFilteringSong.append(song)
-
 for i in range(len(library)):
-    song = {'title':library[i]['title'], 'album': library[i]['album'], 'trackNumber': library[i]['trackNumber']}
+    song = {'title':library[i]['title'].replace('.mp3', ''), 'album': library[i]['album'], 'trackNumber': library[i]['trackNumber']}
     if song in filteringSong:
         if not ('no text' in song['title']) :
             overlapSong.append(library[i]['id'])

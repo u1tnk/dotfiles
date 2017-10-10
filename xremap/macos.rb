18619e42565f5860b29e9fe6252060c5586fc1ac
yuichi tanaka <u1tnk1123@gmail.com>
2017-10-10 20:31:55 +0900

    update xremap config

--- xremap/macos.rb
+++ xremap/macos.rb
@@ -2,8 +2,21 @@
   remap "Alt-#{key}", to: "C-#{key}"
 end
 
+remap "Shift-Alt-bracketright", to: "Ctrl-Page_Down"
+remap "Shift-Alt-bracketleft", to: "Ctrl-Page_Up"
+
 window class_only: 'google-chrome' do
   %w[f].each do |key|
-    remap "Alt-#{key}", to: "C-#{key}"
+    remap "Alt-#{key}", to: "Ctrl-#{key}"
   end
+  remap "Shift-Alt-t", to: "Shift-Ctrl-t"
+end
+
+# see: https://github.com/nonstop/termit/wiki/Termit-Lua-API
+window class_only: 'termit' do
+  remap "Alt-c", to: "Ctrl-Insert"
+  remap "Alt-v", to: "Shift-Insert"
+  remap "Shift-Alt-bracketright", to: "Alt-Right"
+  remap "Shift-Alt-bracketleft", to: "Alt-Left"
+  remap "Alt-w", to: "Shift-Ctrl-w"
 end
--- xremap/start_xremap.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-CURRENT=`dirname $0`
-
-/usr/local/bin/xremap $CURRENT/init.rb >> /tmp/xremap.log 2>&1

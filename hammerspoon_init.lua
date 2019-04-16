110beb9152877c687410d0a3f3bf97eb51836e04
yuichi tanaka <u1tnk1123@gmail.com>
2019-04-16 22:21:55 +0900

    windowsに合わせてopt系のマッピングをヤメ、ctrl+j問題の対応も追加

--- hammerspoon_init.lua
+++ hammerspoon_init.lua
@@ -17,19 +17,19 @@ end
 
 -- カーソル移動
 for k, v in pairs({h='left', j='down', k='up', l='right'}) do
-  remapKey({'alt'}, k, keyCode(v))
-  remapKey({'alt', 'shift'}, k, keyCode(v, {'shift'}))
+  remapKey({'command'}, k, keyCode(v))
+  remapKey({'command', 'shift'}, k, keyCode(v, {'shift'}))
 end
 
 string.format("volume down: %s", hs.audiodevice.defaultOutputDevice():outputVolume())
 -- Volume調整
-hs.hotkey.bind({'alt'}, '-', function()
+hs.hotkey.bind({'command'}, '-', function()
   local new_volume = math.max(0, math.ceil(hs.audiodevice.current().volume - 5))
   hs.alert.show(string.format("volume down: %s", new_volume), 0.5)
   hs.audiodevice.defaultOutputDevice():setVolume(new_volume)
 end)
 
-hs.hotkey.bind({'alt'}, '=', function()
+hs.hotkey.bind({'command'}, '=', function()
   local new_volume = math.min(100, math.ceil(hs.audiodevice.current().volume + 5))
   hs.alert.show(string.format("volume up: %s", new_volume), 0.5)
   hs.audiodevice.defaultOutputDevice():setVolume(new_volume)
@@ -50,14 +50,29 @@ local function getCurrentBrightness()
   return math.ceil(builtin_screen:getBrightness() * 10) / 10
 end
 
-hs.hotkey.bind({'alt'}, '1', function()
+hs.hotkey.bind({'command'}, '1', function()
   local new_brightness = math.max(0,  getCurrentBrightness()- 0.2)
   hs.alert.show(string.format("brightness down: %s", new_brightness), 0.5)
   builtin_screen:setBrightness(new_brightness)
 end)
 
-hs.hotkey.bind({'alt'}, '2', function()
+hs.hotkey.bind({'command'}, '2', function()
   local new_brightness = math.min(1, getCurrentBrightness() + 0.2)
   hs.alert.show(string.format("brightness up: %s", new_brightness), 0.5)
   builtin_screen:setBrightness(new_brightness)
 end)
+
+
+local function handleGlobalAppEvent(name, event, app)
+  if event == hs.application.watcher.activated then
+    if name == 'iTerm2' then
+        hs.hotkey.bind({"ctrl"}, "j", function()
+            hs.eventtap.keyStroke({}, "kana", 0)
+        end)
+    else
+        hs.hotkey.disableAll("ctrl", "j")
+    end
+  end
+end
+appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
+appsWatcher:start()
